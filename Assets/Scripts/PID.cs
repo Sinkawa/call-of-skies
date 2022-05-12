ff0428c68393fe64f3fe1cffd4c86699538ec2a3
Sinkawa <suvoroff.ar@gmail.com>
2022-05-12 15:40:37 +0300

    Updated code files with license notation. Deleted some files.

--- Assets/Scripts/PID.cs
+++ Assets/Scripts/PID.cs
@@ -1,9 +1,25 @@
+// Copyright © 2022 Alexander Suvorov. All rights reserved.
+// 
+// Based on the code from Brian-Stone on the Unity forums
+// https://forum.unity.com/threads/rigidbody-lookat-torque.146625/#post-1005645
+// 
+//
+// This file is part of call-of-skies.
+// 
+// call-of-skies is free software: you can redistribute it and/or modify it under the terms of the GNU General
+// Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+// 
+// call-of-skies is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along with call-of-skies. If not, see
+// <https://www.gnu.org/licenses/>.
+
 using System;
 
-/// <summary>
-/// Based on the code from Brian-Stone on the Unity forums
-/// https://forum.unity.com/threads/rigidbody-lookat-torque.146625/#post-1005645
-/// </summary>
+
 [Serializable] public class PID
 {
     private float _p, _i, _d;
